e949afe0616f7ff9cf1b6806d56f64fbd96dae90
Richard <richard.albornoz@gmail.com>
2020-03-28 20:18:03 -0300

    se agrega el archivo elif

---
+++ elif.py
@@ -0,0 +1,11 @@
+a = 5
+b = 10
+c = 12
+d = 2
+
+if (a > d) and (b < c):
+    print ("es verdadera ambas condiciones con AND")
+elif (b < c):
+    print ("el valor de b es menor que c")
+else:
+    print ("Ninguna de las condiciones anteriores es TRUE")
