4c957c2f8886e8e3d903ee8196bb1f563fec1543
Richard <richard.albornoz@gmail.com>
2020-03-28 21:17:52 -0300

    otros ejemplos del funcionamiento de FOR

--- for.py
+++ for.py
@@ -1,9 +1,22 @@
 colores = ["rojo", "verde", "azul", "negro"]
-
+# for para recorrer listas
 for color in colores:
     print (color)
 
+# for para recorrer un rango automatico
+for numero in range(0,10):
+    print (numero)
+
+# for para recorrer un rango de 2 en 2
+for numero in range(0,10,2):
+    print (numero)
+
+
+# romper un for con el break
 
 for numero in range(0,10):
     print (numero)
+    if (numero == 5):
+        break
+
 
