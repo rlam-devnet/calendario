a6a742b825cda4f518d32bd9bc34d09ca8c0d21e
Richard <richard.albornoz@gmail.com>
2020-03-28 21:37:14 -0300

    se agregan ejemplos de While

---
+++ WHILE/while.py
@@ -0,0 +1,18 @@
+inicio = 1
+final = 10
+
+while (inicio < final):
+    print (inicio)
+    inicio += 1
+
+valor = True
+numero = 1
+
+while (valor):
+    print (numero)
+    numero += 1
+    if (numero == 10):
+        valor = False
+
+
+
